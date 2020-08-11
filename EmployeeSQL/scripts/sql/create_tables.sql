250d73ea9d457f154f44ae05004743eaed54a870
wclewett <wclewett@gmail.com>
2020-08-11 14:25:34 -0400

    Scripts

--- EmployeeSQL/scripts/sql/create_tables.sql
+++ EmployeeSQL/scripts/sql/create_tables.sql
@@ -1,17 +1,22 @@
+-- Create Departments Schema
 create table departments(
 	dept_no varchar(4) primary key,
 	dept_name varchar(30)
 );
 
+-- Import Data and View
 select * from departments;
 
+-- Create Titles Schema
 create table titles(
 	title_id varchar(5) primary key,
 	title varchar(30)
 );
 
+-- Import Data and View
 select * from titles;
 
+-- Create Employees Schema
 create table employees(
 	emp_no int primary key,
 	emp_title_id varchar(5) references titles(title_id),
@@ -22,25 +27,32 @@ create table employees(
 	hire_date date
 );
 
+-- Import Data and View
 select * from employees;
 
+-- Create Department/Employee Schema
 create table dept_emp(
 	emp_no int references employees(emp_no),
 	dept_no varchar(4)
 );
 
+-- Import Data and View
 select * from dept_emp;
 
+-- Create Department Manager Schema
 create table dept_manager(
 	dept_no varchar(4) references departments(dept_no),
 	emp_no int primary key
 );
 
+-- Import Data and View
 select * from dept_manager;
 
+-- Create Salaries Schema
 create table salaries(
 	emp_no int references employees(emp_no),
 	salary int
 );
 
+-- Import Data and View
 select * from salaries;
